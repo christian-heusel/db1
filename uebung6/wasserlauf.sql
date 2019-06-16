2fc156d8467aa2abe0b3ac43c631e815d51060da
Christian Heusel <christian@heusel.eu>
2019-06-16 17:39:50 +0200

    aufgabe 1 & 3
    Signed-off-by: Christian Heusel <christian@heusel.eu>

---
+++ uebung6/wasserlauf.sql
@@ -0,0 +1,64 @@
+create table WASSERLAUF (
+       Fluss varchar(20) primary key, 
+       Muendung varchar(20), 
+       Laenge int
+);
+
+
+insert into WASSERLAUF values (
+       'Donau', 'Schwarzes Meer', '2888'
+);
+insert into WASSERLAUF values (
+	'Elbe','Nordsee','1091'
+);
+insert into WASSERLAUF values (
+       'Fulda','Weser','218'
+);
+insert into WASSERLAUF values (
+       'Havel','Elbe','325'
+);
+insert into WASSERLAUF values (
+        'Ilm','Saale','129'
+);
+insert into WASSERLAUF values (
+       'Inn','Donau','517'
+);
+insert into WASSERLAUF values (
+	'Isar','Donau','286'
+);
+insert into WASSERLAUF values (
+	'Lech','Donau','264'
+);
+insert into WASSERLAUF values (
+	'Main','Rhein','524'
+);
+insert into WASSERLAUF values (
+	'Oder','Ostsee','866'
+);
+insert into WASSERLAUF values (
+	'Rhein','Nordsee','1320'
+);
+insert into WASSERLAUF values (
+	'Saale','Elbe','413'
+);
+insert into WASSERLAUF values (
+	'Schorte','Ilm','9'
+);
+insert into WASSERLAUF values (
+	'Schwarza','Saale','53'
+);
+insert into WASSERLAUF values (
+	'Spree','Havel','382'
+);
+insert into WASSERLAUF values (
+	'Werra','Weser','292'
+);
+insert into WASSERLAUF values (
+	'Weser','Nordsee','452'
+);
+insert into WASSERLAUF values (
+	'Neckar','Rhein','367'
+);
+insert into WASSERLAUF values (
+	'Elsenz','Neckar','53'
+);
